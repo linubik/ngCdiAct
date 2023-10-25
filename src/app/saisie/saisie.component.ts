0c82fbccb8d5228ff92c6b40b1930e66305e307c
linubik <linubik@aliceadsl.fr>
2023-10-25 20:33:19 +0200

    new components

---
+++ src/app/saisie/saisie.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saisie',
+  templateUrl: './saisie.component.html',
+  styleUrls: ['./saisie.component.css']
+})
+export class SaisieComponent {
+
+}
